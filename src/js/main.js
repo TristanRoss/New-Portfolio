7bd1e941f7e0f1def2a0052672770928ed9604f8
TristanRoss <tgr1998@live.com>
2019-07-08 23:32:19 -0700

    More animations

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,10 @@ let welcomeText = document.getElementById("welcome");
 
 anime({
     targets: welcomeText,
-    translateY: [
-        {value: -50, duration: 0, delay: 200},
-        {value: 0, duration: 1000}
-    ],
-    // easing: 'easeInOutSine',
-    // direction: 'reverse'
+    opacity: 0,
+    duration: 8000,
+    easing: 'easeInExpo',
+    direction: 'reverse'
     // rotate: {
     //     // value: '1turn',
     //     easing: 'easeInOutSine'
@@ -50,3 +48,88 @@ anime({
     easing: 'linear',
     loop: true
 });
+
+
+let projOneGit = document.getElementById("projOneGit");
+
+anime({
+    targets: projOneGit,
+    translateX: [
+        {value: 70, duration: 1000, delay: 200},
+        {value: 0, duration: 1000},
+        {value: 0, duration: 1000}
+    ],
+    easing: 'linear',
+    loop: true
+});
+
+let projOneLive = document.getElementById("projOneLive");
+
+anime({
+    targets: projOneLive,
+    translateX: [
+        {value: 130, duration: 0, delay: 200},
+        {value: 70, duration: 1000},
+        {value: 130, duration: 1000},
+        {value: 130, duration: 1000}
+    ],
+    easing: 'linear',
+    loop: true
+});
+
+let projTwoGit = document.getElementById("projTwoGit");
+
+anime({
+    targets: projTwoGit,
+    translateX: [
+        {value: -70, duration: 1000, delay: 200},
+        {value: 0, duration: 1000},
+        {value: 0, duration: 1000}
+    ],
+    easing: 'linear',
+    loop: true
+});
+
+let projTwoLive = document.getElementById("projTwoLive");
+
+anime({
+    targets: projTwoLive,
+    translateX: [
+        {value: -118, duration: 0, delay: 200},
+        {value: -70, duration: 1000},
+        {value: -118, duration: 1000},
+        {value: -118, duration: 1000}
+    ],
+    easing: 'linear',
+    loop: true
+});
+
+let projThreeGit = document.getElementById("projThreeGit");
+
+anime({
+    targets: projThreeGit,
+    translateX: [
+        {value: 70, duration: 1000, delay: 200},
+        {value: 0, duration: 1000},
+        {value: 0, duration: 1000}
+    ],
+    easing: 'linear',
+    loop: true
+});
+
+let projThreeLive = document.getElementById("projThreeLive");
+
+anime({
+    targets: projThreeLive,
+    translateX: [
+        {value: 130, duration: 0, delay: 200},
+        {value: 70, duration: 1000},
+        {value: 130, duration: 1000},
+        {value: 130, duration: 1000}
+    ],
+    easing: 'linear',
+    loop: true
+});
+
+
+
