dbf62f7e7d8dca639648eb50916db315f7ed834a
TristanRoss <tgr1998@live.com>
2019-07-08 21:01:26 -0700

    First animations

---
+++ src/js/main.js
@@ -0,0 +1,47 @@
+let welcomeText = document.getElementById("welcome");
+
+anime({
+    targets: welcomeText,
+    translateY: [
+        {value: -50, duration: 0, delay: 200},
+        {value: 0, duration: 1000}
+    ],
+    // easing: 'easeInOutSine',
+    // direction: 'reverse'
+    // rotate: {
+    //     // value: '1turn',
+    //     easing: 'easeInOutSine'
+    // }
+});
+
+let linkedin = document.getElementById("linkedin");
+
+anime({
+    targets: linkedin,
+    translateY: [
+        {value: 0, duration: 0, delay: 100},
+        {value: 80, duration: 2000},
+        {value: 0, duration: 2000, delay: 2100}
+    ],
+    translateX: [
+        {value: 100, duration: 2000, delay: 2100},
+        {value: 0, duration: 2000, delay: 2100}
+    ],
+    easing: 'linear'
+});
+
+let github = document.getElementById("github");
+
+anime({
+    targets: github,
+    translateY: [
+        {value: 0, duration: 0, delay: 4100},
+        {value: 80, duration: 2000},
+        {value: 0, duration: 2000, delay: 2100}
+    ],
+    translateX: [
+        {value: -100, duration: 2000, delay: 2100},
+        {value: 0, duration: 2000, delay: 2100}
+    ],
+    easing: 'linear'
+});
